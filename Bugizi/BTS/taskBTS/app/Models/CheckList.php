5b50342831c188fe168a10aa9489105a1421ad22
ekosuprianto96 <ekhosaputra23@gmail.com>
2022-10-04 17:58:02 +0700

    Tugas API

---
+++ Bugizi/BTS/taskBTS/app/Models/CheckList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CheckList extends Model
+{
+    use HasFactory;
+
+    protected $table = 'checklist';
+    protected $guarded = ['id'];
+}
